fba2b7df4acac7415a3cdb179b222315e983c876
Rafael Carvalho <crcarvalho@outlook.com>
2019-01-10 17:57:50 -0300

    Anotacao com multiplos mapeamentos de url

--- src/br/com/alura/gerenciador/servlet/OiMundo.java
+++ src/br/com/alura/gerenciador/servlet/OiMundo.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/oi")
+@WebServlet(urlPatterns= {"/oi", "/hello"})
 public class OiMundo extends HttpServlet {
 	
 	@Override
