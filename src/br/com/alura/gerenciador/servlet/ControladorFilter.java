dffff9a0cdfd2a416189b049f5b8d6ca8636f345
Rafael Carvalho <crcarvalho@outlook.com>
2019-01-24 16:43:46 -0300

    Adicionando init e destroy aos filtros para atender ao jetty

--- src/br/com/alura/gerenciador/servlet/ControladorFilter.java
+++ src/br/com/alura/gerenciador/servlet/ControladorFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -17,6 +18,12 @@ import br.com.alura.gerenciador.acao.Acao;
 
 //@WebFilter(urlPatterns="/entrada")
 public class ControladorFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
 
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
